8aba01dd2b42406cf6c99925bac0d19cd86a3f6e
Nikita Pushchin <vbnekit@p5y.su>
2023-07-13 18:38:30 +0300

    `_common.asynchronous_tools` module added

---
+++ src/p5/libvirt_lxc_helper/_common/asynchronous_tools/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+assert "__main__" != __name__
+
+
+def _private():
+    from .. import module_helpers as _module_helpers_module
+
+    _make_lazy_getter = _module_helpers_module.lazy_attributes.make_getter
+
+    class _Result(object):
+        lazy_getter = _make_lazy_getter(dictionary = {})
+
+    return _Result
+
+
+_private = _private()
+
+__all__ = _private.lazy_getter.keys
+__date__ = None
+__author__ = None
+__version__ = None
+__credits__ = None
+_fields = tuple()
+__bases__ = tuple()
+
+
+def __getattr__(name: str): return _private.lazy_getter(name = name)
